3b24724b29f069dfd8da921c8a5c3a824ed48c7e
clayton_michael <clayton.michael@uconn.edu>
2017-07-25 12:26:08 -0400

    Added jQuery functionality to expand, add, and remove forms

--- CustomModule/custom_module/custom_module.js
+++ CustomModule/custom_module/custom_module.js
@@ -5,37 +5,91 @@ jQuery(document).ready(function () {
     jQuery('.form-item-commonGardenSelect').addClass('commonGardenClass');
     jQuery('.form-item-plantationSelect').addClass('plantationClass');
     jQuery('.form-item-natPopSelect').addClass('natPopClass');
-
-
-    jQuery('#submitAjaxButton').click(function () {
-
-        // alert('You clicked on the ajax button');
-        jQuery.ajax({
-
-            url: Drupal.settings.basePath + 'getHello',
-            data: {
-
-                name: jQuery("#edit-name").val(),
-                token: myToken
-
-            },
-
-            success: function (data) {
-
-                jQuery("#msg-display-area").empty();
-                jQuery("#msg-display-area").append(data);
-                jQuery('#edit-study').hide();
-
-            }
-
-        });
-
-    });
-
-    //function below hides and shows menus according to user selection
+    jQuery('#edit-species').clone().appendTo('#edit-species');
     
-//    jQuery('#edit-study').change(function () {
+//    jQuery('#submitAjaxButton').click(function () {
 //
+//        alert('You clicked on the ajax button');
+//        jQuery.ajax({
+//
+//            url: Drupal.settings.basePath + 'getHello',
+//            data: {
+//
+//                name: jQuery("#edit-name").val(),
+//                token: myToken
+//
+//            },
+//
+//            success: function (data) {
+//
+//                jQuery("#msg-display-area").empty();
+//                jQuery("#msg-display-area").append(data);
+//                jQuery('#edit-study').hide();
+//
+//            }
+//
+//
+//        });
+//
+//    });
+    
+    var numAuthors = 1;
+
+    jQuery('#AddAuthor').click(function()
+        {
+            var newAuthorForm = jQuery('#edit-secondaryauthor').clone().attr('id', '#edit-secondaryauthor' + numAuthors);
+            
+            newAuthorForm.insertAfter('#edit-secondaryauthor');
+   
+            var newButton = jQuery('#AddAuthor').clone().attr('id', '#AddAuthor' + numAuthors);
+            
+            newButton.click(function(){
+                jQuery(newAuthorForm).remove();                
+                jQuery(newButton).remove();   
+            });
+            
+            newButton.insertAfter('#edit-secondaryauthor');
+            
+        }
+            );
+    
+    var oldNumSpecies = jQuery('#edit-speciesnumber').val();
+
+    jQuery('#edit-speciesnumber').change(function(){
+        if (jQuery('#edit-speciesnumber').val() > oldNumSpecies){
+            var currentNumSpecies = jQuery('#edit-speciesnumber').val();
+            
+            for(var i = oldNumSpecies; i < currentNumSpecies; i++){
+                var newSpeciesForm = jQuery('#edit-species').clone().attr('id', '#edit-species' + i);
+                
+                newSpeciesForm.change(function(){
+                    if (jQuery('#edit-speciesnumber').val() ){
+//                        remove the Form
+                    }                    
+                })
+                        
+                newSpeciesForm.insertAfter('#edit-species');
+            }
+            
+            oldNumSpecies = currentNumSpecies;
+        }
+        
+        else if (jQuery('#edit-speciesnumber').val() < oldNumSpecies){
+            var currentNumSpecies = jQuery('#edit-speciesnumber').val();
+            
+            for(var i = oldNumSpecies; i >= currentNumSpecies; --i){
+                jQuery('#edit-species1').remove();
+            }
+            
+            oldNumSpecies = currentNumSpecies;``
+        }
+        else{
+        }
+    });
+    
+//    push these changes please!!
+        
+//    jQuery('#edit-study').change(function () {
 //        if (jQuery('#edit-study').val() == 1)
 //        {
 //            jQuery('.commonGardenClass').show();
@@ -52,8 +106,7 @@ jQuery(document).ready(function () {
 //            jQuery('.plantationClass').hide();
 //            jQuery('.natPopClass').show();
 //        }
-//
 //    }
 //            );
 
-});
+});  
