b5904ef8a0144ef6bae4d83914c375c90a83f19b
Peter Richter <peter.richter@uconn.edu>
2018-07-25 09:58:23 -0400

    removed unnecessary fieldset from organism

--- page_1.php
+++ page_1.php
@@ -340,15 +340,10 @@ function page_1_create_form(&$form, $form_state){
         for($i = 1; $i <= 5; $i++){
             
             $form['organism']["$i"] = array(
-              '#type' => 'fieldset',
-              //'#title' => t("Tree Species $i:"),
-            );
-            
-            $form['organism']["$i"]['species'] = array(
               '#type' => 'textfield',
               '#title' => t("Species $i:"),
               '#autocomplete_path' => "species/autocomplete",
-              '#default_value' => isset($values['organism']["$i"]['species']) ? $values['organism']["$i"]['species'] : NULL,
+              '#default_value' => isset($values['organism']["$i"]) ? $values['organism']["$i"] : NULL,
               '#attributes' => array(
                 'data-toggle' => array('tooltip'),
                 'data-placement' => array('left'),
@@ -499,10 +494,10 @@ function page_1_validate_form(&$form, &$form_state){
         }
         
         for ($i = 1; $i <= $organism_number; $i++){
-            $name = $organism[$i]['species'];
+            $name = $organism[$i];
             
             if ($name == ''){
-                form_set_error("organism[$i][species", "Tree Species $i: field is required.");
+                form_set_error("organism[$i", "Tree Species $i: field is required.");
             }
             else{
                 $name = explode(" ", $name);
@@ -512,7 +507,7 @@ function page_1_validate_form(&$form, &$form_state){
                 $empty_pattern = '/^ *$/';
                 $correct_pattern = '/^[A-Z|a-z|.| ]+$/';
                 if (!isset($genus) or !isset($species) or preg_match($empty_pattern, $genus) or preg_match($empty_pattern, $species) or !preg_match($correct_pattern, $genus) or !preg_match($correct_pattern, $species)){
-                    form_set_error("organism[$i][species", check_plain("Tree Species $i: please provide both genus and species in the form \"<genus> <species>\""));
+                    form_set_error("organism[$i", check_plain("Tree Species $i: please provide both genus and species in the form \"<genus> <species>\""));
                 }
             }
         }
