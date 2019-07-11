f7f1a7fea7ff91a0f6b97f5f97fdfc2e90daae01
PeterRichter <peter.richter@uconn.edu>
2019-07-11 12:51:57 -0400

    Add support for submitting haploid/diploid SSRs As per #40. Still need to add support for polyploid.

--- forms/submit/submit_all.php
+++ forms/submit/submit_all.php
@@ -44,7 +44,7 @@ function tpps_submit_all($accession) {
   // For simplicity and efficiency, all fourth page submissions take place in
   // the TPPS File Parsing Tripal Job.
   $includes = array();
-  $includes[] = module_load_include('module', 'tpps');
+  $includes[] = module_load_include('inc', 'tpps', 'includes/file_parsing');
   $args = array($form_state['accession']);
   $jid = tripal_add_job("TPPS File Parsing - {$form_state['accession']}", 'tpps', 'tpps_file_parsing', $args, $uid, 10, $includes, TRUE);
   $form_state['job_id'] = $jid;
