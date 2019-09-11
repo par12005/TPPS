106d2547dce0237eaec247799e0be3a01b1e2118
PeterRichter <peter.richter@uconn.edu>
2019-09-11 16:19:17 -0400

    Add NCBI API Key config option

--- admin/config.php
+++ admin/config.php
@@ -30,6 +30,12 @@ function tpps_admin_settings(array $form, array &$form_state) {
     '#default_value' => variable_get('tpps_maps_api_key', NULL),
   );
 
+  $form['tpps_ncbi_api_key'] = array(
+    '#type' => 'textfield',
+    '#title' => t('TPPS NCBI EUtils API Key'),
+    '#default' => variable_get('tpps_ncbi_api_key', NULL),
+  );
+
   $form['tpps_geocode_api_key'] = array(
     '#type' => 'textfield',
     '#title' => t('TPPS OpenCage Geocoding API Key'),
