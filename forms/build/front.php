d45a2b096275dffdf1536733a9d0ffcb7677bd2e
PeterRichter <peter.richter@uconn.edu>
2019-11-19 15:50:47 -0500

    Center landing page figure

--- forms/build/front.php
+++ forms/build/front.php
@@ -70,7 +70,7 @@ function tpps_front_create_form(array &$form, array $form_state) {
   $image_path = drupal_get_path('module', 'tpps') . '/images/';
   $prefix_text = "<div>Welcome to TPPS!<br><br>The Tripal PopGen Submit (TPPS) workflow provides researchers with a streamlined submission interface for studies resulting from any combination of genotype, phenotype, and environmental data for georeferenced forest trees. ";
   $prefix_text .= "You can <a href='/tpps_details/main'><b>view completed studies here</b></a> or pick up where you left off by using the dropdown box found <a href='#tpps_begin_options'><b>here</b></a> (below). Visit the <a href='https://tpps.readthedocs.io/en/latest/index.html'><b>full manual</b></a> for more details and help.<br />";
-  $prefix_text .= "<figure><img style=\"max-height:100%;max-width:100%;\" src=\"{$image_path}TPPS_front_diagram.png\"></figure>";
+  $prefix_text .= "<figure style=\"text-align:center;\"><img style=\"max-height:100%;max-width:100%;\" src=\"{$image_path}TPPS_front_diagram.png\"></figure>";
   $prefix_text .= "TPPS has documentation to assist users with the process of creating a submission, which can be accessed <a target=\"blank\" href=\"https://tpps.readthedocs.io/en/latest/user.html\">here</a>.<br><br></div>";
   $prefix_text .= "<div id='tpps_begin_options'>";
   $suffix_text = "</div>";
