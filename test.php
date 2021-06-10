d4d310d59790157c085cab4efc7d4b147f04aca2
Peter Richter <peter.richter@uconn.edu>
2021-06-10 09:13:56 -0400

    Remove test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$path = '/var/www/html/sites/default/files/tpps_genotype/a.tar.gz';
-$unzip_dir = '/var/www/html/sites/default/files/tpps_genotype/tmp';
-$zip = new \PharData($path);
-#$zip->decompress();
-#$zip->extractTo($unzip_dir);
-print_r("test\n");
-
