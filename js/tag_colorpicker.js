40e980869912ed63c2ba38c1dfb0a68bf802a287
PeterRichter <peter.richter@uconn.edu>
2021-08-19 10:30:15 -0400

    Code styling

--- js/tag_colorpicker.js
+++ js/tag_colorpicker.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function ($) {
-    $('#edit-color').colorPicker({
-        opacity: false, // disables opacity slider
-        renderCallback: function($elm, toggled) {
-            $elm.val('#' + this.color.colors.HEX);
-        }
-    });
+  $('#edit-color').colorPicker({
+    opacity: false, // disables opacity slider
+    renderCallback: function($elm, toggled) {
+      $elm.val('#' + this.color.colors.HEX);
+    }
+  });
 });
