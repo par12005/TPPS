1032af875b3a9ac2e445707ef942c0dfadcb4240
PeterRichter <peter.richter@uconn.edu>
2021-08-19 10:47:28 -0400

    Change color picker to toggle

--- js/tag_colorpicker.js
+++ js/tag_colorpicker.js
@@ -1,8 +1,22 @@
 jQuery(document).ready(function ($) {
-  $('#edit-color').colorPicker({
-    opacity: false, // disables opacity slider
-    renderCallback: function($elm, toggled) {
-      $elm.val('#' + this.color.colors.HEX);
+  var picker = false;
+  $('#color-picker').click(function() {
+    if (!picker) {
+      console.log('adding color picker');
+      $('#edit-color').colorPicker({
+        opacity: false, // disables opacity slider
+      });
+      picker = true;
     }
+    else {
+      console.log('removing color picker');
+      $('.cp-color-picker').remove();
+      picker = false;
+    }
+  });
+
+  jQuery('#edit-color').css('background-color', jQuery('#edit-color')[0].value);
+  $('#edit-color').on('input', function() {
+    jQuery('#edit-color').css('background-color', jQuery('#edit-color')[0].value);
   });
 });
