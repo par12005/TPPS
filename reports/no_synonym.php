e954dc82be73544a81a24bebb43e3e889d4ad4ce
Vlad Savitsky <vlad.savitsky@gmail.com>
2023-03-10 15:43:07 -0500

    #3kged9b Added report menu items for 'No Synonym' page and 'Custom Unit'.

---
+++ reports/no_synonym.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Generates list of Phenotypes without Synonyms.
+ */
+
+function tpps_admin_no_synonym() {
+  //module_load_include('inc', 'tpps', 'reports/simple_report');
+
+  return array();
+}
+
---
+++ reports/unit_warning.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Generates list of Phenotypes which Unit differ from Synonym's Unit.
+ */
+
+
+// @TODO Remove selected items from db table.
+function tpps_admin_unit_warning() {
+  return 'Unit';
+}
+
