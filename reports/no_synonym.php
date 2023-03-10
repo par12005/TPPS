c319f02ed426f1fa3b99eaed91b89c2710a480c1
Vlad Savitsky <vlad.savitsky@gmail.com>
2023-03-10 17:19:49 -0500

    #3kged9b Added custom module 'simple_report' and added simple integration with it.

--- reports/no_synonym.php
+++ reports/no_synonym.php
@@ -5,9 +5,8 @@
  * Generates list of Phenotypes without Synonyms.
  */
 
-function tpps_admin_no_synonym() {
-  //module_load_include('inc', 'tpps', 'reports/simple_report');
-
-  return array();
+function tpps_admin_no_synonym_report() {
+  $table_name = 'tpps_phenotype_unit_warning';
+  return simple_table_report($table_name);
 }
 
--- reports/unit_warning.php
+++ reports/unit_warning.php
@@ -7,7 +7,8 @@
 
 
 // @TODO Remove selected items from db table.
-function tpps_admin_unit_warning() {
-  return 'Unit';
+function tpps_admin_unit_warning_report() {
+  $table_name = 'tpps_phenotype_unit_warning';
+  return simple_table_report($table_name);
 }
 
