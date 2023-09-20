52eea356cae73da72d5696e5ad6bd9dc83aa6679
Vlad Savitsky <vlad.savitsky@gmail.com>
2023-09-20 14:31:44 +0300

    #861n70hh6 Added clearing form if no DOI information found.

--- js/tpps_page_1.js
+++ js/tpps_page_1.js
@@ -4,6 +4,8 @@
  * TPPS Page 1 form specific JS-code.
  */
 (function($, Drupal) {
+  var doiSelector = '#edit-doi';
+  var doiMessageBox = '#doi-message';
 
   // Prepare place to temporary store request's data.
   // Usage: console.log(Drupal.tpps.doi);
@@ -42,7 +44,7 @@
    * @return bool
    *   Returns TRUE if validation passed and FALSE otherwise.
    */
-  function validateStrings(string) {
+  function tppsDoiValidate(string) {
     // @TODO [VS] Minor. Get this validation regex from Drupal.settings.tpps.
     var pattern = /^10\.\d{4,9}[\-._;()\/:A-Za-z0-9]+$/;
     return $.trim(string).match(pattern) ? true : false;
@@ -58,25 +60,25 @@
    * 'error' = red
    * 'warning' = yellow.
    *
-   * @param string selector
-   *   jQuery selector of element to show messages.
+   * @param string doiSelector
+   *   jQuery doiSelector of element to show messages.
    * @param array data
    *   Messages. Keys are: 'errors', 'warnings' and 'statuses'.
    *   WARNING:
    *   Values are arrays (not strings).
    */
   function tppsShowMessages(selector, data) {
-    var $messageBox = $(selector);
+    var $doiMessageBox = $(selector);
     if (data.errors !== undefined) {
-      $messageBox.append('<div class="error">'
+      $doiMessageBox.append('<div class="error">'
         + data.errors.join('</div><div class="error">') + '</div>');
     }
     if (data.warnings !== undefined) {
-      $messageBox.append('<div class="warning">'
+      $doiMessageBox.append('<div class="warning">'
         + data.warnings.join('</div><div class="warning">') + '</div>');
     }
     if (data.statuses !== undefined) {
-      $messageBox.append('<div class="status">'
+      $doiMessageBox.append('<div class="status">'
         + data.statuses.join('</div><div class="status">') + '</div>');
     }
   }
@@ -94,7 +96,78 @@
     $(selector).prop('disabled', true);
   }
 
+  /**
+   * tppsDoiFill
+   *
+   * @param data $data
+   * @access public
+   * @return void
+   */
+  function tppsDoiFill(data) {
+    if (typeof (data) != 'undefined') {
+      // Fill text fields.
+      $('#edit-publication-primaryauthor').val(data.doi_info.primary ?? '');
+      $('#edit-publication-abstract').val(data.doi_info.abstract ?? '');
+      // Default value for year is '0' ('- Select -').
+      $('#edit-publication-year').val(data.doi_info.year ?? 0);
+      $('#edit-publication-title').val(data.doi_info.title ?? '');
+      // @TODO Get Journal field value.
+      // $('#edit-publication-journal').val(data.doi_info.journal);
 
+      if (!data.success) {
+        // Clear all fields.
+        $('input[name="publication[secondaryAuthors][number]"]').val(0);
+        $('input[id^="edit-publication-secondaryauthors-remove"]').mousedown();
+        $('input[name="organism[1][name]"]').val('');
+        $('input[name="organism[number]"]').val(1);
+        $('input[id^="edit-organism-remove"]').mousedown();
+        return;
+      }
+      // ::::::::::::::::::::::::::::::::::::::::::::::::::::::
+      // Secondary Authors.
+      if (data.doi_info.secondaryNumber > 0) {
+        // @TODO Probably outdated. Check it.
+        $('input[name="publication[secondaryAuthors][check]"]')
+          .val(data.doi_info.secondaryCheck)
+        $('input[name="publication[secondaryAuthors][number]"]').val(
+          parseInt(data.doi_info.secondaryNumber) - 1
+        );
+        $('input[id^="edit-publication-secondaryauthors-add"]').mousedown();
+        $.each(data.doi_info.secondary, function( key, value ) {
+          waitForElm('input[name="publication[secondaryAuthors]['
+            + ( key + 1 ) + ']"]')
+            .then((elm) => { $(elm).val(value); }
+          );
+        });
+      }
+      // Organisms.
+      if (data.doi_info.speciesNumber >= 1) {
+        // @TODO Reuse empty fields.
+        // @TODO Not overwrite 1st field if it's not empty.
+        // 1st field is present at form and can't be removed.
+        // So it must be populated manually.
+        if (typeof data.doi_info.species[0] == 'undefined') {
+          $('input[name="organism[1][name]"]').val('');
+        }
+        else {
+          $('input[name="organism[1][name]"]').val(
+            data.doi_info.species[0]
+          );
+        }
+        // Add necessary number of fields.
+        $('input[name="organism[number]"]')
+            .val( data.doi_info.speciesNumber - 1 );
+        $('input[id^="edit-organism-add"]').mousedown();
+        // Populate new fields.
+        $.each(data.doi_info.species, function( key, value ) {
+          waitForElm('input[name="organism[' + ( key + 1 ) + '][name]"]')
+            .then((elm) => { $(elm).val(value); }
+          );
+        });
+      }
+    }
+  }
+  // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   Drupal.behaviors.tpps_page_1 = {
     attach: function (context, settings) {
 
@@ -114,11 +187,9 @@
         // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
         // 'Publication DOI' field change.
         if (typeof(settings.tpps.ajaxUrl) !== 'undefined') {
-          var selector = '#edit-doi';
-          var messageBox = '#doi-message';
 
-          $(selector).blur(function(e) {
-            tppsFieldDisable(selector);
+          $(doiSelector).blur(function(e) {
+            tppsFieldDisable(doiSelector);
 
             // @TODO Check if DOI value really was changed.
             var doi = $(this).val();
@@ -128,8 +199,7 @@
               typeof (Drupal.tpps.doiLastValue) != 'undefined'
               && Drupal.tpps.doiLastValue == doi
             ) {
-              tppsFieldEnable(selector);
-              console.log('DOI value is the same');
+              tppsFieldEnable(doiSelector);
               return;
             }
             else {
@@ -137,29 +207,30 @@
               Drupal.tpps.doiLastValue = doi;
             }
 
-            if (!validateStrings(doi)) {
-              $(messageBox).empty();
+            if (!tppsDoiValidate(doi)) {
+              $(doiMessageBox).empty();
               var data = {
                 "errors": [
                   Drupal.t('Invalid DOI format. Example DOI: 10.1111/dryad.111')
                 ]
               };
-              tppsShowMessages(messageBox, data);
-              tppsFieldEnable(selector);
+              tppsShowMessages(doiMessageBox, data);
+              tppsFieldEnable(doiSelector);
               return;
             }
 
             // Check if we have cached result first.
             if (Drupal.settings.tpps.cache && typeof (Drupal.tpps.doi[doi]) != 'undefined') {
-              $(messageBox).empty();
+              $(doiMessageBox).empty();
               var data = Drupal.tpps.doi[doi];
-              tppsShowMessages(messageBox, data);
-              tppsFieldEnable(selector);
+              tppsShowMessages(doiMessageBox, data);
+              tppsFieldEnable(doiSelector);
+              tppsDoiFill(data);
             }
             else {
               var url = settings.basePath + settings.tpps.ajaxUrl + '/get_doi';
               // Remove existing messages.
-              $(messageBox).empty();
+              $(doiMessageBox).empty();
               $.ajax({
                 method: 'post',
                 data: {'doi': doi},
@@ -168,7 +239,7 @@
                   // User changed DOI during AJAX-request.
                   if (tppsWasDoiChanged(doi)) { return; }
                   // Server/Network errors.
-                  tppsShowMessages(messageBox, [{
+                  tppsShowMessages(doiMessageBox, [{
                     "errors": [
                       Drupal.t("DOI value wasn't completed")
                     ]
@@ -176,91 +247,21 @@
                   var errorMessage = jqXHR.status + " " + jqXHR.statusText
                     + "\n\n" + jqXHR.responseText;
                   console.log(errorMessage);
-                  tppsFieldEnable(selector);
+                  tppsFieldEnable(doiSelector);
                 },
-                success: function (data) {
-
+                success: function(data) {
                   // Store DOI check result to avoid multiple requests.
-                  // @TODO Check if data not empty.
-                  if (typeof (data) !== 'undefined') {
-                    Drupal.tpps.doi[doi] = data;
-                    // User changed DOI during AJAX-request.
-                    if (tppsWasDoiChanged(doi)) { return; }
-                    tppsShowMessages(messageBox, data);
-                    tppsFieldEnable(selector);
-                    // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-                    // Store DOI check result to avoid multiple requests.
-                    // @TODO Check if data not empty.
-                    if (typeof (data) != 'undefined' && data.success) {
-                      // ::::::::::::::::::::::::::::::::::::::::::::::::::::::
-                      // Fill text fields.
-                      if (!$('#edit-publication-primaryauthor').val()) {
-                        $('#edit-publication-primaryauthor').val(data.doi_info.primary);
-                      }
-                      if (!$('#edit-publication-abstract').val()) {
-                        $('#edit-publication-abstract').val(data.doi_info.abstract);
-                      }
-                      // Default value for year is '0' ('- Select -').
-                      if (
-                        $('#edit-publication-year').val() == 0
-                        || !$('#edit-publication-year').val()
-                      ) {
-                        $('#edit-publication-year').val(data.doi_info.year);
-                      }
-                      if (!$('#edit-publication-title').val()) {
-                        $('#edit-publication-title').val(data.doi_info.title);
-                      }
-                      // @TODO Get Journal field value.
-                      // $('#edit-publication-journal').val(data.doi_info.journal);
-
-                      // ::::::::::::::::::::::::::::::::::::::::::::::::::::::
-                      // Secondary Authors.
-                      if (data.doi_info.secondaryNumber >= 1) {
-                        // @TODO Probably outdated. Check it.
-                        $('input[name="publication[secondaryAuthors][check]"]')
-                          .val(data.doi_info.secondaryCheck)
-                        // @TODO Reuse empty fields.
-                        $('input[name="publication[secondaryAuthors][number]"]').val(
-                          parseInt(data.doi_info.secondaryNumber) - 1
-                        );
-                        $('input[id^="edit-publication-secondaryauthors-add"]').mousedown();
-                        $.each(data.doi_info.secondary, function( key, value ) {
-                          waitForElm('input[name="publication[secondaryAuthors]['
-                            + ( key + 1 ) + ']"]')
-                            .then((elm) => { $(elm).val(value); }
-                          );
-                        });
-                      }
-                      // Organisms.
-                      if (data.doi_info.speciesNumber >= 1) {
-                        // @TODO Reuse empty fields.
-                        // @TODO Not overwrite 1st field if it's not empty.
-                        // 1st field is present at form and can't be removed.
-                        // So it must be populated manually.
-                        $('input[name="organism[1][name]"]').val(
-                          data.doi_info.species[0]
-                        );
-                        // Add necessary number of fields.
-                        $('input[name="organism[number]"]')
-                            .val( data.doi_info.speciesNumber - 1 );
-                        $('input[id^="edit-organism-add"]').mousedown();
-                        // Populate new fields.
-                        $.each(data.doi_info.species, function( key, value ) {
-                          waitForElm('input[name="organism[' + ( key + 1 ) + '][name]"]')
-                            .then((elm) => { $(elm).val(value); }
-                          );
-                        });
-                      }
-                    }
-                    // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-                  }
-                  else {
+                  Drupal.tpps.doi[doi] = data;
+                  // User changed DOI during AJAX-request.
+                  if (tppsWasDoiChanged(doi)) { return; }
+                  if (typeof (data) == 'undefined') {
                     var data = [{
                       "errors": [Drupal.t('Received empty response.')]
                     }];
-                    tppsShowMessages(messageBox, data);
-                    return;
                   }
+                  tppsShowMessages(doiMessageBox, data);
+                  tppsFieldEnable(doiSelector);
+                  tppsDoiFill(data);
                 }
               });
             }
