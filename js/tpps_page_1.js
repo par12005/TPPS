b9d0b326d394cc7df2e4b8d3f13e2ff40eebb0c9
Vlad Savitsky <vlad.savitsky@gmail.com>
2023-10-10 11:16:52 +0300

    #86ayecjxq Fixed duplication of asterisks for required fields on Page 1 when status of publication changed.

--- js/tpps_page_1.js
+++ js/tpps_page_1.js
@@ -255,12 +255,17 @@
       let $label = $('#edit-publication-' + publicationFieldList[i])
         .parents('.form-item').find('label');
       if ($label.length) {
-        $label.html($label.html()  + ' *');
+        $label.html(
+          // To avoid duplication of asterisk for field which was hidden
+          // but Drupal Form API States we force removement of existing
+          // asterisks (if any). It's better then duplicate Drupal Form
+          // API States and track field's visability.
+          $label.html().replace(' *', '')
+          + ' *'
+        );
       }
     }
   }
-
-
 
   // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   // Behavior.
