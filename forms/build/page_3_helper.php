f6450eb9e1276445ba8feaef3675c394ab166533
Vlad Savitsky <vlad.savitsky@gmail.com>
2023-10-30 00:45:57 +0200

    #86aygr2q7 WIP. Replaced drupal_add_js() with ['#attached'] for Page 3 location.

--- forms/build/page_3_helper.php
+++ forms/build/page_3_helper.php
@@ -129,9 +129,14 @@ function tpps_study_location(array &$form, array &$form_state) {
       $map_api_tools = "<script src=\"https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js\"></script><script src=\"https://maps.googleapis.com/maps/api/js?key=$map_api_key&callback=initMap\"
       async defer></script>"
       . "<div id=\"_map_wrapper\"></div>";
-      drupal_add_js(array('tpps' => array('tree_info' => $coords)), 'setting');
-      drupal_add_js(array('tpps' => array('study_locations' => TRUE)), 'setting');
-
+      //drupal_add_js(array('tpps' => array('tree_info' => $coords)), 'setting');
+      //drupal_add_js(array('tpps' => array('study_locations' => TRUE)), 'setting');
+      $form['#attached']['js'][] = [
+        'type' => 'setting',
+        'data' => [
+          'tpps' => ['study_locations' => TRUE, 'tree_info' => $coords],
+        ],
+      ];
       $form['study_location']['map-button']['#suffix'] = $map_api_tools;
     }
   }
