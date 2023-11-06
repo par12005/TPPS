3588e83ed5c21df827120f943773016096f35a75
Vlad Savitsky <vlad.savitsky@gmail.com>
2023-11-06 17:51:09 +0200

    #86ayj4mc5 Improved look and feel of the 'File Diagnostics' page.

--- admin/pages.php
+++ admin/pages.php
@@ -87,26 +87,24 @@ function tpps_admin_files_diagnostics_page(array $form, array &$form_state, $stu
   $markup .= '<div style="display: inline-block; width: 30%; vertical-align: top;">';
   $markup .= '<h4>Project ID Managed</h4>';
   foreach ($project_file_ids as $fid) {
-    $markup .= '<div>' . $fid . '-';
     $file = file_load($fid);
     if ($file) {
       $file_url = check_plain(file_create_url($file->uri));
-      $markup .= "<a href='$file_url' target='blank'>$file->filename</a>";
+      $markup .= '<div>' . $fid . ' - <a href="' . $file_url .
+        '" target="blank">' . $file->filename . '</a></div>';
     }
-    $markup .= '</div>';
   }
   $markup .= '</div>';
 
   $markup .= '<div style="display: inline-block; width: 30%; vertical-align: top;">';
   $markup .= '<h4>Submission state</h4>';
   foreach ($file_ids as $fid) {
-    $markup .= '<div>' . $fid . '-';
     $file = file_load($fid);
     if ($file) {
       $file_url = check_plain(file_create_url($file->uri));
-      $markup .= "<a href='$file_url' target='blank'>$file->filename</a>";
+      $markup .= '<div>' . $fid . ' - <a href="' . $file_url .
+        '" target="blank">' . $file->filename . '</a></div>';
     }
-    $markup .= '</div>';
   }
   $markup .= '</div>';
 
@@ -115,22 +113,14 @@ function tpps_admin_files_diagnostics_page(array $form, array &$form_state, $stu
   $overall_file_ids = $serialized_data['files'] ?? [];
   sort($overall_file_ids);
   foreach ($overall_file_ids as $fid) {
-    $markup .= '<div>' . $fid . '-';
     $file = file_load($fid);
     if ($file) {
       $file_url = check_plain(file_create_url($file->uri));
-      $markup .= "<a href='$file_url' target='blank'>$file->filename</a>";
+      $markup .= '<div>' . $fid . ' - <a href="' . $file_url .
+        '" target="blank">' . $file->filename . '</a></div>';
     }
-    $markup .= '</div>';
-
   }
   $markup .= '</div>';
-  $markup .= '</div>';
-
-  $form['markup'] = array(
-    '#type' => 'markup',
-    '#markup' => $markup
-  );
-
+  $form['markup'] = ['#type' => 'markup', '#markup' => $markup];
   return $form;
 }
