7796a7bab8e0b57c6fc4f2577ea204322f99355b
Vlad Savitsky <vlad.savitsky@gmail.com>
2024-03-25 00:51:30 +0200

    #86ayw8a7k Fixed JS error on 'tpps/details/%' page which blocked Google Map showing.

--- js/tpps.js
+++ js/tpps.js
@@ -645,7 +645,14 @@ function initMap() {
       maps[fid + '_total_long'];
     });
   }
-  if (window.location.pathname.match(detail_regex) || (typeof Drupal.settings.tpps.study_locations !== 'undefined')) {
+  if (
+    typeof Drupal.settings.tpps != 'undefined'
+    && typeof Drupal.settings.tpps.tree_info != 'undefined'
+    && (
+      window.location.pathname.match(detail_regex)
+      || typeof Drupal.settings.tpps.study_locations !== 'undefined'
+    )
+  ) {
     maps[''] = new google.maps.Map(document.getElementById('_map_wrapper'), {
       center: {lat:0, lng:0},
       zoom: 5,
