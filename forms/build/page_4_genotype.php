08025b636a06e9c70398adce035f09f22b434089
Vlad Savitsky <vlad.savitsky@gmail.com>
2024-07-14 23:49:53 +0000

    VS/ Fix VCF-files pre-validation button

--- forms/build/page_4_genotype.php
+++ forms/build/page_4_genotype.php
@@ -1003,7 +1003,6 @@ function tpps_add_dropdown_file_selector(array $form_bus) {
       'local' => t('My @file_name is stored locally', $params),
       'remote' => t('My @file_name is stored at @hostname', $params),
     ],
-    '#default_value' => '',
     '#states' => [
       'visible' => [
         $snps_fieldset_name . '[genotyping-type]"]' =>
@@ -1013,6 +1012,7 @@ function tpps_add_dropdown_file_selector(array $form_bus) {
       ],
     ],
   ];
+
   // Field was relocated (v.2).
   // ['saved_values', TPPS_PAGE_4, 'organism-' . $i, 'genotype', 'files'] =>
   // ['saved_values', TPPS_PAGE_4, 'organism-' . $i, 'genotype', $snps_fieldset].
