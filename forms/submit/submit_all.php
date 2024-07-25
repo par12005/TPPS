3554b43707be30f5ca45e89acddb5ac9556a71d3
Risharde Ramnath <risharde@gmail.com>
2024-07-25 15:28:51 -0400

    Hotfix to only save the first part of species in either cases of submit all pipeline

--- forms/submit/submit_all.php
+++ forms/submit/submit_all.php
@@ -372,6 +372,7 @@ function tpps_submit_page_1(array &$shared_state, TripalJob &$job = NULL) {
     $parts_count = count($parts);
     if (isset($parts[2]) and ($parts[2] == 'var.' or $parts[2] == 'subsp.' or $parts[2] == 'spp.' or $parts[2] == 'sp.')) {
       $infra = implode(" ", array_slice($parts, 2));
+      $species = $parts[1]; // Based on Emily's suggestion 7/25/2024
     }
     else if (isset($parts[2]) and $parts_count <= 3) {
       // cater for examples like Taxus baccata L or Taxus baccata L.
