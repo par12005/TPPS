cc110504f2a0c5f916185f374669a8c2e43ec14e
Vlad Savitsky <vlad.savitsky@gmail.com>
2024-12-16 22:27:40 +0200

    Fixed broken feature 'Fieldset collapsed status' and added to TPPS Admin Panel.

--- js/tpps_fieldset.js
+++ js/tpps_fieldset.js
@@ -15,15 +15,30 @@
           return;
         }
         let fieldsetId = '#edit-' + fieldsetName.replace(/_/g, '-');
-        $(fieldsetId + ' legend a.fieldset-title').on('click', function(e) {
-          let phpFieldsetName = $(this).parents('fieldset').attr('id')
-            .replace('edit-', '').replace(/-/g, '_');
-          // @TODO Get field name from settings.
-          let hiddenFieldName = Drupal.settings.tpps.fieldsetCollapsedStatus.prefix
-            + phpFieldsetName;
-          let $hiddenField = $("[name*='" + hiddenFieldName + "']");
-          $hiddenField.val(($hiddenField.val() == 1 ? 0 : 1));
-        });
+        $(fieldsetId + ' legend a.fieldset-title')
+          .on('click', function(e) {
+            // Store via form submit.
+            let phpFieldsetName = $(this).parents('fieldset').attr('id')
+              .replace('edit-', '').replace(/-/g, '_');
+            // @TODO Get field name from settings.
+            let hiddenFieldName = Drupal.settings.tpps.fieldsetCollapsedStatus.prefix
+              + phpFieldsetName;
+            let $hiddenField = $("[name*='" + hiddenFieldName + "']");
+            $hiddenField.val(($hiddenField.val() == 1 ? 0 : 1));
+            // Store via ajax-request to server side.
+            $.ajax({
+                url: Drupal.settings.tpps.fieldsetCollapsedStatus.ajaxUrl,
+                method: 'post',
+                dataType: 'json',
+                data: {
+                  'name': hiddenFieldName,
+                  'value': $hiddenField.val(),
+                },
+                success: function(data){
+                  console.log(data);
+                },
+            });
+          });
       });
     }
   };
