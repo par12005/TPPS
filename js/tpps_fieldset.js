8bf1957bc0111cc59a025a352b10cae19af24222
Vlad Savitsky <vlad.savitsky@gmail.com>
2024-12-18 10:44:14 +0200

    Updated 'Fieldset collapsed state' feature to use only AJAX-requests and remove hidden form element.

--- js/tpps_fieldset.js
+++ js/tpps_fieldset.js
@@ -1,44 +1,48 @@
 /**
  * @file
  *
- * TPPS Fieldset Collapsed Status.
+ * TPPS Fieldset Collapsed State.
  *
  * Remembers status of the fieldset when form submitted.
  */
 (function ($) {
-  Drupal.behaviors.tppsFieldsetCollapsedStatus = {
+  Drupal.behaviors.tppsFieldsetCollapsedState = {
     attach: function (context) {
-      // Allows to store status of the fieldset (collapsed or not).
-      $('.' + Drupal.settings.tpps.fieldsetCollapsedStatus.class, context).each(function() {
-        let fieldsetName = $(this).attr('fieldset');
-        if (typeof fieldsetName == 'undefined' || !fieldsetName.length) {
-          return;
+      /**
+       * Stores state of the fieldset.
+       *
+       * @param object element
+       *   JQuery objec of the element.
+       */
+      function storeState(element, op) {
+        let $fieldset = element.parents('fieldset');
+        let value = $fieldset.hasClass('collapsed');
+        // Strange but values are reverted. Probably because we check
+        // before it was changed by Drupal API.
+        if (op == 'click') {
+          value = !value;
         }
-        let fieldsetId = '#edit-' + fieldsetName.replace(/_/g, '-');
-        $(fieldsetId + ' legend a.fieldset-title')
-          .on('click', function(e) {
-            // Store via form submit.
-            let phpFieldsetName = $(this).parents('fieldset').attr('id')
-              .replace('edit-', '').replace(/-/g, '_');
-            // @TODO Get field name from settings.
-            let hiddenFieldName = Drupal.settings.tpps.fieldsetCollapsedStatus.prefix
-              + phpFieldsetName;
-            let $hiddenField = $("[name*='" + hiddenFieldName + "']");
-            $hiddenField.val(($hiddenField.val() == 1 ? 0 : 1));
-            // Store via ajax-request to server side.
-            $.ajax({
-                url: Drupal.settings.tpps.fieldsetCollapsedStatus.ajaxUrl,
-                method: 'post',
-                dataType: 'json',
-                data: {
-                  'name': hiddenFieldName,
-                  'value': $hiddenField.val(),
-                },
-                success: function(data){
-                  console.log(data);
-                },
-            });
-          });
+        console.log(value);
+        $.ajax({
+            url: Drupal.settings.tpps.fieldsetCollapsedState.ajaxUrl,
+            method: 'post',
+            dataType: 'json',
+            data: {
+              'name': $fieldset.attr('id'),
+              'value': value ? 1 : 0,
+            },
+        });
+      }
+
+      // Store fieldset state (collapsed or expanded).
+      $('fieldset.collapsible legend a.fieldset-title', context)
+        // 'click' event can't be used because fieldset not have 'collapsed'
+        // class yet.
+        .on('mouseup keyup', function() {
+          storeState($(this), 'click');
+      })
+      .each(function() {
+        storeState($(this), 'load');
       });
     }
   };
