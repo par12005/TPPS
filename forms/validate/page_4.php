a490ee1f38a4757722b36eb5f49f5704cfc5e010
Vlad Savitsky <vlad.savitsky@gmail.com>
2024-12-18 19:05:14 +0200

    Fixed validation of format 'start:stop' for position column of SNP Association file to allow both 'start:stop' format and  integer.

--- forms/validate/page_4.php
+++ forms/validate/page_4.php
@@ -1009,14 +1009,15 @@ function tpps_validate_genotype_snps(array &$genotype, $org_num, array $form, ar
             $position_col = $groups['Position'][3];
             $positions = tpps_parse_file_column($assoc_file, $position_col, $assoc_no_header);
             foreach ($positions as $position) {
-              if (!preg_match('/^(\d+):(\d+)$/', $position)) {
-                form_set_error("$id][genotype][$snps_fieldset][snps-association",
-                  t('SNP Association File: We detected SNP positions that do '
-                  . 'not match the required format. '
-                  . 'The correct format is: "start:stop".')
-                );
-                break;
+              if (is_int($position) || preg_match('/^(\d+):(\d+)$/', $position)) {
+                continue;
               }
+              form_set_error("$id][genotype][$snps_fieldset][snps-association",
+                t('SNP Association File: We detected SNP positions that do '
+                . 'not match the required format. '
+                . 'The correct format is: "start:stop" or integer .')
+              );
+              break;
             }
           }
 
