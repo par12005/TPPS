8025e8be29b3d2b7ee9073cbe14d4a1b486d99a2
Vlad Savitsky <vlad.savitsky@gmail.com>
2025-08-11 18:10:50 +0300

    Moved changes from 'VS/assay_design_file_columns' branch in file: - forms/submit/submit_all.php Added helper functions tpps_log_header() and tpps_log_line(). Updated AssayDesign::process() and processRow() with code from submit_all.php script.

--- forms/submit/submit_all.php
+++ forms/submit/submit_all.php
@@ -12,9 +12,11 @@
 // Global variables.
 $tpps_job_logger = NULL;
 $tpps_job = NULL;
-module_load_include('inc', 'tpps', 'src/SnpAssociation.class');
-module_load_include('inc', 'tpps', 'src/PhenotypeMeta.class');
-module_load_include('inc', 'tpps', 'src/PhenotypeData.class');
+
+// @TODO Check if class autoloaded in CLI and remove this code.
+//module_load_include('inc', 'tpps', 'src/SnpAssociation.class');
+//module_load_include('inc', 'tpps', 'src/PhenotypeMeta.class');
+//module_load_include('inc', 'tpps', 'src/PhenotypeData.class');
 
 /**
  * Initialized the job logger which handles writing to job logs
@@ -2278,6 +2280,14 @@ function tpps_submit_genotype(array &$shared_state, array $species_codes, $i, Tr
     $genotype_count = 0;
   }
 
+  AssayDesign::process($i, $shared_state, $options);
+
+throw new Exception('update');
+// @todo Was in 7.x-1.x-dev but I moved to AssayDesign::process and lost
+// call of the
+// tpps_generate_vcf_from_assay_and_assay_design($options, $shared_state);
+
+if (0) {
   // This if statement caters for the Genotype Assay Design file
   // (which holds extra data like positions etc)
   // This is usually also accompanied with the Genotype SNP Assay
@@ -2432,9 +2442,11 @@ function tpps_submit_genotype(array &$shared_state, array $species_codes, $i, Tr
 
 
   }
-
+}
   // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   // 'SSRs' and 'cpSSR' fields.
+  // WARNING:
+  // SSR and cpSSR files could have the same data type for multiple columns.
   foreach (['ssrs', 'ssrs_extra'] as $ssr_field_name) {
     if (!empty($ssr_fid = $genotype['files'][$ssr_field_name])) {
       $options['type'] = 'ssrs';
@@ -8041,218 +8053,6 @@ function tpps_process_genotype_spreadsheet($row, array &$options = []) {
 }
 
 /**
- * Processes genotype SNP design assay file.
- *
- * Initially done to get position data from the assay design file.
- */
-function tpps_process_genotype_snp_assay_design($row, array &$options = array()) {
-  $line_rows = $row;
-  $analysis_id = $options['analysis_id']; // needed to lookup source features
-  $headers = &$options['headers'];
-  $records = &$options['records'];
-  $columns = $options['file_columns'];
-  $organism_index = $options['organism_index'];
-  $seq_var_cvterm = $options['seq_var_cvterm'];
-  // print_r("File columns: ");
-  // print_r($columns);
-  // print_r("\n");
-  // print_r("Data in row:");
-  // print_r($line_rows);
-  // print_r("\n");
-
-  $chr_name = $line_rows[$columns['chr']];
-  // // if the scaffold is only the number, we append scaffold_ to it (TGDR665)
-  // if (is_numeric($chr_name)) {
-  //   $chr_name = "scaffold_" . $chr_name;
-  // }
-  // // if TGDR665, replace chr with scaffold_
-  // if (substr($chr_name,0,3) == 'chr') {
-  //   $chr_name = str_replace('chr', 'scaffold_', $chr_name);
-  // }
-
-  $marker_name_raw = $line_rows[$columns['marker_name']];
-  $position = intval($line_rows[$columns['position']]);
-  $marker_type = $options['marker_type']; // we could force 'SNP' here
-
-
-  // RISH NOTES: This addition uses the organism_id based on the organism order
-  // of the fourth page (we likely have to pass the i from previous function here)
-  // THIS TECHNICALLY OVERRIDES PETER'S LOGIC ABOVE. TO BE DETERMINED IF RISH'S WAY IS CORRECT
-  // OR NOT [6/22/2023]
-  // THIS WAS AN ISSUE BROUGHT UP BY EMILY REGARDING SNPS NOT BEING ASSOCIATED WITH POP TRICH (665 STUDY)
-  $species_codes = $options['species_codes'];
-  $species_code = null;
-  $organism_id = null;
-  $count_tmp = 0;
-  foreach ($species_codes as $organism_id_tmp => $species_code_tmp) {
-    $count_tmp = $count_tmp + 1; // increment
-    // Check if count_tmp matches $organism_index
-    if ($count_tmp == $organism_index) {
-      $species_code = $species_code_tmp;
-      $organism_id = $organism_id_tmp;
-      break;
-    }
-  }
-
-  $srcfeature_id = NULL;
-  // Get the srcfeature_id
-  $srcfeature_results = chado_query('select feature.feature_id from chado.feature
-    join chado.analysisfeature on feature.feature_id = analysisfeature.feature_id
-    where feature.name = :chr_name and analysisfeature.analysis_id = :analysis_id',
-    [
-      ':chr_name' => $chr_name,
-      ':analysis_id' => $analysis_id
-    ]
-  );
-
-  foreach ($srcfeature_results as $row) {
-    $srcfeature_id = $row->feature_id;
-  }
-
-  if ($srcfeature_id != NULL) {
-    echo "[GOOD] srcfeature_id for $chr_name: " . $srcfeature_id . "\n";
-
-    $marker_name = $marker_name_raw . $marker_type;
-
-    // We need to find the current marker_name in the feature table
-    $results = chado_query("SELECT * FROM chado.feature WHERE uniquename = :uniquename", [
-      ':uniquename' => $marker_name
-    ]);
-    $feature_id = NULL;
-    foreach ($results as $feature) {
-      $feature_id = $feature->feature_id;
-    }
-
-    if ($feature_id == NULL) {
-      // We should add the marker (marker_name)
-      // $marker_name
-      chado_insert_record('feature', [
-        'name' => $marker_name,
-        'organism_id' => $organism_id,
-        'uniquename' => $marker_name,
-        'type_id' => $seq_var_cvterm,
-      ]);
-
-      // Recheck for the feature_id
-      $results = chado_query("SELECT * FROM chado.feature WHERE uniquename = :uniquename", [
-        ':uniquename' => $marker_name
-      ]);
-      foreach ($results as $feature) {
-        $feature_id = $feature->feature_id;
-      }
-    }
-
-    if ($feature_id != NULL) {
-      echo "[GOOD] Marker name $marker_name has feature_id: $feature_id\n";
-
-      // Before we add a new featureloc record, check to make sure one does not already exist
-      // in the featureloc table since we don't currently delete previous featurelocs on
-      // study reloads
-      $featureloc_results = chado_query('SELECT count(*) as c1 FROM chado.featureloc
-        WHERE feature_id = :feature_id AND srcfeature_id = :srcfeature_id;', [
-          ':feature_id' => $feature_id,
-          ':srcfeature_id' => $srcfeature_id
-        ]
-      );
-      $featureloc_count = 0;
-      foreach ($featureloc_results as $row) {
-        $featureloc_count = $row->c1;
-      }
-      // This means no featureloc exists, so insert it
-      if ($featureloc_count == 0) {
-        // Check for indels (longer than 1 reads)
-        $snp = trim($line_rows[$columns['snp']]);
-        $snp_possible_reads = explode('/', $snp);
-        $read_length = 0; // default for non-indels
-        foreach ($snp_possible_reads as $read) {
-          $tmp_read_length = strlen($read);
-          if ($tmp_read_length > 1) { // only for indels, we need a read_length of more than 0
-            if ($tmp_read_length > $read_length) {
-              $read_length = $tmp_read_length;
-            }
-          }
-        }
-
-        // TODO: if read_length is more than 1, it is an indel, change marker type
-        // Also check code in tpps_process_genotype_sheet to check this or else
-        // these 2 things will cause a submit failure - after conference (6/15/2023)
-
-        $records['featureloc'][$marker_name] = [
-          'fmin' => $position,
-          'fmax' => ($position + $read_length),
-          'srcfeature_id' => $srcfeature_id,
-          'feature_id' => $feature_id,
-        ];
-        print_r($records['featureloc'][$marker_name]);
-      }
-      else {
-        echo "[GOOD ALTERNATIVE] Featureloc record already exists, no need to add\n";
-      }
-
-      // Check if forward sequence information has been added
-      $forward_sequence_cvterm_id = NULL;
-      // Get cvterm_id (assuming it exists)
-      $results = chado_query("SELECT * FROM chado.cvterm
-        WHERE name = 'five_prime_flanking_region' LIMIT 1;", []);
-      foreach ($results as $row) {
-        $forward_sequence_cvterm_id = $row->cvterm_id;
-      }
-
-      // Check if record already exists
-      $results = chado_query("SELECT count(*) as c1 FROM chado.featureprop
-        WHERE feature_id = :feature_id AND type_id = :type_id;", [
-          ':feature_id' => $feature_id,
-          ':type_id' => $forward_sequence_cvterm_id
-      ]);
-      $count = $results->fetchObject()->c1;
-      // If record not found
-      if ($count == 0) {
-        // add to record to featureprop table
-        $records['featureprop'][$feature_id . $forward_sequence_cvterm_id] = [
-          'feature_id' => $feature_id,
-          'type_id' => $forward_sequence_cvterm_id,
-          'value' => $line_rows[$columns['forward_sequence']]
-        ];
-      }
-
-
-      // Check if reverse sequence information has been added
-      $reverse_sequence_cvterm_id = NULL;
-      // Get cvterm_id (assuming it exists)
-      $results = chado_query("SELECT * FROM chado.cvterm
-        WHERE name = 'three_prime_flanking_region' LIMIT 1;", []);
-      foreach ($results as $row) {
-        $reverse_sequence_cvterm_id = $row->cvterm_id;
-      }
-
-      // Check if record already exists
-      $results = chado_query("SELECT count(*) as c1 FROM chado.featureprop
-        WHERE feature_id = :feature_id AND type_id = :type_id;", [
-          ':feature_id' => $feature_id,
-          ':type_id' => $reverse_sequence_cvterm_id
-      ]);
-      $count = $results->fetchObject()->c1;
-      // If record not found
-      if ($count == 0) {
-        // add record to featureprop table
-        $records['featureprop'][$feature_id . $reverse_sequence_cvterm_id] = [
-          'feature_id' => $feature_id,
-          'type_id' => $reverse_sequence_cvterm_id,
-          'value' => $line_rows[$columns['reverse_sequence']]
-        ];
-      }
-    }
-    else {
-      echo "[ERROR] Marker name $marker_name feature_id could not be found\n";
-    }
-  }
-  else {
-    tpps_log("srcfeature_id for $chr_name could not be found - we cannot add featureloc data.",
-      [], TRIPAL_ERROR);
-  }
-}
-
-/**
  * This function formats headers for a microsatellite spreadsheet.
  *
  * SSR/cpSSR spreadsheets will often have blank or duplicate headers, depending
@@ -9692,6 +9492,28 @@ function tpps_log($message, $variables = [], $severity = TRIPAL_INFO) {
     catch (Error $err) {
     }
   }
+}
+
+/**
+ * Adds visible messages to separated sections in log messages.
+ *
+ * @param mixed $message
+ *   Header text.
+ */
+function tpps_log_header($message) {
+  tpps_log_line();
+  tpps_log($message);
+  tpps_log_line();
+}
+
+/**
+ * Prints out line in logs.
+ *
+ * Note: This line could be used as a separator between sections.
+ */
+function tpps_log_line($char = '-') {
+  $string_length = 80;
+  tpps_log("\n" . str_repeat($char, $string_length) . "\n");
 }
 
 /**
