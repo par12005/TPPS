2678bfaa1dad05ff7f4ee0189116aa4232ecda1d
Vlad Savitsky <vlad.savitsky@gmail.com>
2025-11-03 12:20:52 +0200

    Hotfix: Correct clearance of the DB when pipeline processing failed and 'project_id' wasn't saved.

--- forms/submit/submit_all.php
+++ forms/submit/submit_all.php
@@ -172,6 +172,21 @@ function tpps_submit_all($accession, TripalJob $job = NULL) {
       ['@pid' => $submission->sharedState['ids']['project_id']],
       TRIPAL_INFO
     );
+    // When Tripal job finished with error (wasn't complete with suceess)
+    // then SharedState['ids']['project_id'] will be empty and function
+    // tpps_submission_clear_db() will not clear correctly DB.
+    // Those not removed records lead to errors like:
+    // "DETAIL:  Key (project_id, fid)=(3560, 21149) already exists.
+    // [site https://tgwebdev.cam.uchc.edu] [TRIPAL ERROR] [TRIPAL_JOB] [ERROR]
+    // Error message: SQLSTATE[23505]: Unique violation: 7 ERROR:  duplicate
+    // key value violates unique constraint
+    // "tpps_project_file_managed_tpps_project_file_managed_c1_key"DETAIL:
+    // Key (project_id, fid)=(3560, 21149) already exists."
+    //
+    // Other solution is to call function tpps_submission_clear_db() but to
+    // get latest changes we need to save current state of the $submission object.
+    tpps_project_clear_db($submission->sharedState['ids']['project_id']);
+
     tpps_tripal_entity_publish('Project',
       [
         $submission->sharedState['title'],
@@ -5066,11 +5081,11 @@ function tpps_genotype_vcf_processing_batch_some_features_insert(&$settings, $cu
     }
     // $sql .= ' ON CONFLICT (organism_id, uniquename, type_id) DO UPDATE SET uniquename=EXCLUDED.uniquename RETURNING feature_id, uniquename';
     // $sql .= ' ON CONFLICT (organism_id, uniquename) where type_id = 1491 DO UPDATE SET uniquename=EXCLUDED.uniquename RETURNING feature_id, uniquename';
-    
-    // This gives error: 
+
+    // This gives error:
     $sql .= ' ON CONFLICT (organism_id, uniquename) DO UPDATE SET uniquename=EXCLUDED.uniquename, type_id=EXCLUDED.type_id RETURNING feature_id, uniquename';
 
-    
+
     if ($variant_index > 0) {
       $results_feature_inserts = db_query($sql);
     }
