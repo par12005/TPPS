d817aed8e95ab4a12b7fa74fd1620e8dd5d645f7
Vlad Savitsky <vlad.savitsky@gmail.com>
2026-03-10 20:12:29 +0200

    Implemented dynamic building iframe URL using JS. Implemented GoogleScholar::getQuery() and ::getEndpointUrl().

---
+++ js/tpps_doi_lookup.js
@@ -0,0 +1,49 @@
+/**
+ * @file
+ *
+ * TPPS DOI Lookup.
+ */
+(function ($) {
+  Drupal.behaviors.doi_lookup = {
+    attach: function (context, settings) {
+      // TPPS DOI Lookup.
+      // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+      let doi_field_selector = settings.tpps.doi_lookup.field;
+      $(doi_field_selector).blur(function() {
+        let doi = $(this).val();
+        let $iframe=$('#google-scholar');
+        let proxy_url = buildUrl (settings, doi);
+        $iframe.attr('src', proxy_url).innerHTML(proxy_url);
+      });
+
+      /**
+       * Build URL for Google Scholar iFrame.
+       *
+       * @param object $settings
+       *   Drupal.settings.
+       * @param string $doi
+       *   DOI
+       *
+       * @return string
+       *   Retuns URL with proxy (CORS anywhere).
+       */
+      function buildUrl (settings, doi) {
+        let query = settings.tpps.doi_lookup.query;
+        query["q"] = doi;
+        return settings.tpps.doi_lookup.proxy
+          + settings.tpps.doi_lookup.endpoint + '?' + $.param(query);
+      }
+
+      // Get Google Scholar iframe content.
+      $('iframe#google-scholar').on('load', function() {
+        // The iframe and its contents have finished loading
+        var $iframeContents = $(this).contents();
+// @TODO Send content to backend.
+        //
+        //$iframeContents.find("body").append("<p>Content added after load.</p>");
+      });
+
+      // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    }
+  }
+})(jQuery, Drupal);
