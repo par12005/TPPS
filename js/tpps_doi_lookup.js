89ab2091503bffa7cc659a94906b4fa8dc6f3d3f
Vlad Savitsky <vlad.savitsky@gmail.com>
2026-03-12 16:29:30 +0200

    Implemented getting previosly saved publication information for DOI. Implemented showing publication data at test page. Implmented correct URL for iframe and etc.

--- js/tpps_doi_lookup.js
+++ js/tpps_doi_lookup.js
@@ -4,6 +4,9 @@
  * TPPS DOI Lookup.
  */
 (function ($) {
+  Drupal.tpps = Drupal.tpps || {};
+  Drupal.tpps.publicationDOI = Drupal.tpps.publicationDOI || {};
+
   Drupal.behaviors.doi_lookup = {
     attach: function (context, settings) {
       // TPPS DOI Lookup.
@@ -11,14 +14,59 @@
       let doi_field_selector = settings.tpps.doi_lookup.field;
       $(doi_field_selector).blur(function() {
         let doi = $(this).val();
+        $('#dumpContainer').html('').hide();
+        // 1. Request publication data from PublicationDOI::TABLE.
+        $.ajax({
+          url: settings.tpps.doi_lookup.ajax_path,
+          type: 'POST',
+          contentType: 'application/json',
+          dataType: 'json',
+          data: JSON.stringify({"doi": doi}),
+          success: function(response) {
+console.log('Success:', response);
+            if (response.publication_data.length !== 0) {
+              Drupal.tpps.publicationDOI[doi] = response.publication_data;
+              // Show at page.
+              var jsonString = JSON.stringify(Drupal.tpps.publicationDOI[doi], null, 2);
+              $('#dumpContainer').html(
+                '<pre style="margin: 5px; color: white !important;">'
+                + jsonString + '</pre>'
+              ).show();
+
+
+            }
+            else {
+              // Calculate delay.
+              let delay = 0;
+              let last_request = settings.tpps.doi_lookup.last_request;
+              if (last_request) {
+                delay = (last_request + (settings.tpps.doi_lookup.delay * 1000)) - $.now();
+                if (delay < 0) {
+                  delay = 0;
+                }
+              }
+console.log(delay);
+              // Build iframe for Google Scholar SERP.
+              setTimeout(function() {
+                settings.tpps.doi_lookup.last_request = $.now();
+                // Build iframe.
+console.log('build iframe.');
+                let $iframe=$(settings.tpps.doi_lookup.iframe);
+                let proxy_url = buildUrl(settings, doi);
+console.log(proxy_url);
+                if (proxy_url) {
+                  $iframe.attr('src', proxy_url);
+                  // .innerHTML(proxy_url);
+                }
+              }, delay);
+            }
+          },
+          error: function(xhr, status, error) {
+            console.error('Error:', error);
+          }
+        });
 
         // @TODO
-        // 1. Request publication data from PublicationDOI::TABLE.
-        // 2. If there is no data - check delay.
-        let delay = settings.tpps.doi_lookup.delay;
-
-
-
         // 3. Build new url and create iframe.
         // 4. Get iframe content and send to backend.
         //    - store into 'tpps_google_scholar' table.
@@ -29,9 +77,6 @@
         //
 
 
-        let $iframe=$('#google-scholar');
-        let proxy_url = buildUrl (settings, doi);
-        $iframe.attr('src', proxy_url).innerHTML(proxy_url);
       });
 
       /**
@@ -46,20 +91,23 @@
        *   Retuns URL with proxy (CORS anywhere).
        */
       function buildUrl (settings, doi) {
+        let url = '';
         let query = settings.tpps.doi_lookup.query;
         query["q"] = doi;
-        return settings.tpps.doi_lookup.proxy
-          + settings.tpps.doi_lookup.endpoint + '?' + $.param(query);
+        if (settings.tpps.doi_lookup.proxy != '') {
+          url = settings.tpps.doi_lookup.proxy
+            + settings.tpps.doi_lookup.debug
+            + settings.tpps.doi_lookup.endpoint
+            + '?' + $.param(query);
+        }
+        return url;
       }
 
       // Get Google Scholar iframe content.
-      $('iframe#google-scholar').on('load', function() {
+      $(settings.tpps.doi_lookup.iframe).on('load', function() {
         // The iframe and its contents have finished loading
-        let $iframeContents = $(this).contents();
-
-
-
-
+        let iframeContent = $(this).contents().find('html').html();
+        console.log(iframeContent);
 // @TODO Send content to backend.
         //
         //$iframeContents.find("body").append("<p>Content added after load.</p>");
